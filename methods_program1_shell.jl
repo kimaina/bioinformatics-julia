45f7fba237d1df5559aef01416cfd7cbdd67ee03
Neil Sarkar <neil_sarkar@brown.edu>
2018-02-07 20:23:53 -0500

    commit of prog1 shell

---
+++ methods_program1_shell.jl
@@ -0,0 +1,48 @@
+#
+#  methods_program1.jl
+#  First program written in Julia.
+#
+#  Created by BCBI on 2017-02-06.
+#  Last Updated by BCBI on 2018-02-05.
+#  Copyright 2018 Brown University. All rights reserved.
+#
+
+# introduce script
+
+
+# ask if the user is having a nice day
+
+
+# get response from <STDIN>
+
+
+# tell user what they said
+
+
+# ask user for a number, chomp it, then convert to integer
+
+
+# double number
+
+
+# report twice the number
+
+
+# indicate if twice value is larger than 5
+
+
+#### BEGIN IN-CLASS CLASS EXERCISE ####
+
+# have the computer ask for your name
+
+
+# have the computer print out your name
+
+
+# ask user for a number >> convert to Float64
+
+
+# if number is greater than 10 divide by 2, and add 3
+
+
+# otherwise, tell user the number is less than 10
