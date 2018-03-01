d9d930494f49f75da450739ed125520aea22eaef
Liz Chen <liz_chen@brown.edu>
2018-03-01 10:10:56 -0500

    program 4 shell

---
+++ methods_program4_shell.jl
@@ -0,0 +1,61 @@
+#
+#  methods_program4.jl
+#  File Input/Output and Functions
+#
+#  Created by BCBI on 2018-02-26.
+#  Last Updated by BCBI on 2018-02-28.
+#  Copyright 2018 Brown University. All rights reserved.
+#
+
+# open the Adult census income dataset (Adult.txt)
+# https://archive.ics.uci.edu/ml/datasets/adult
+
+# absolute path of file
+
+
+# relative path of file
+
+
+# initialize variables/collections
+
+
+# define function for printing dictionary sorted by key or value
+# ***will fill this in at the end***
+
+
+# read in each line and extract the gender column, keeping track of counts
+
+
+  # skip first line
+
+
+  # skip empty lines
+
+
+  # split the line into an array, based on delimiter (comma followed by space)
+
+
+  # tabulate the counts for gender
+
+
+# report total counts
+
+
+# report total counts by key, in reverse order
+
+
+# report total counts by value, in reverse order (send this output to file)
+
+
+# close files
+
+
+#### BEGIN CLASS EXERCISE
+# write a program to determine the counts of marital status for
+# those with Bachelors level education in Adult.txt;
+# put output into: <netid>_program4_output2.txt
+
+
+
+# use functions for printing
+# ***will fill this in at the end***
